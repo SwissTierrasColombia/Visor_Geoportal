038c4c9de6bde507f781f464ef98308f3bd22a4e
Jorge Useche <naturalmentejorge@gmail.com>
2017-10-13 11:15:10 -0500

    Se automatiza exitosamente contenedor para base de datos.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,11 @@
+# link: https://github.com/kartoza/docker-postgis/blob/develop/Dockerfile
 FROM kartoza/postgis
+RUN apt-get -qqy update && apt-get -qqy --no-install-recommends install \
+	screen \
+	reptyr \
+	&& rm -rf /var/lib/apt/lists/* /var/cache/apt/*
 ADD sql /sql
 #WORKDIR /sql
 RUN chmod 0755 /sql/restore_backup.sh
+# link: http://goinbigdata.com/docker-run-vs-cmd-vs-entrypoint/
 CMD /sql/restore_backup.sh
--- sql/restore_backup.sh
+++ sql/restore_backup.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
-# link: https://github.com/kartoza/docker-postgis/blob/develop/start-postgis.sh
-/bin/bash /start-postgis.sh &
 
-PID=$!
+# rationale: wait for database and execute population scripts for database
+exec_scripts_db(){
 
 # wait for postgres to come up
+printf "Waiting for postgres (localhost-only)."
 until `nc -z 127.0.0.1 5432`; do
-    echo "$(date) - waiting for postgres (localhost-only)..."
+    printf "."
     sleep 1
 done
-echo "postgres ready"
+printf "\npostgres ready\n"
 
-echo "$POSTGRES_DBNAME"
 # Set a default database name
 if [ -z "$POSTGRES_DBNAME" ]; then
   POSTGRES_DBNAME=gis
 fi
 
 # rationale: Wait for database become available
-while ! su - postgres -c "psql -d $POSTGRES_DBNAME -l" &> /dev/null
+printf "Wait a moment while creating the database $POSTGRES_DBNAME."
+while ! su - postgres -c "psql -d $POSTGRES_DBNAME -l &> /dev/null"
 do
-  echo "Wait a moment while loading the database."
+  printf "."
   sleep 2
 done
-echo "done creation"
+printf "\ndone creation\n"
 
-su - postgres -c "psql -f /sql/creation_scripts.sql -d $POSTGRES_DBNAME"
-su - postgres -c "psql -f /sql/newScript.sql -d $POSTGRES_DBNAME"
+#su - postgres -c "psql -f /sql/creation_scripts.sql -d $POSTGRES_DBNAME"
+#su - postgres -c "psql -f /sql/newScript.sql -d $POSTGRES_DBNAME"
+
+}
+
+# rationale: jobs, fb and bg cannot be used in no full interactive shell
+# link: https://askubuntu.com/questions/192798/reading-the-output-from-any-process-using-its-pid
+echo "Execute script."
+exec_scripts_db &
+#PID=$!
+#reptyr -T $PID
+
+# rationale: execute original CMD entry
+# link: https://github.com/kartoza/docker-postgis/blob/develop/start-postgis.sh
+/start-postgis.sh
